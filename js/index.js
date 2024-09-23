6d2d6e3809d4239151279aa75d73ebfd8f002848
M-RayhanKhan <rayhansmartcode@gmail.com>
2024-09-23 12:03:20 +0600

    setAttribute practice

--- js/index.js
+++ js/index.js
@@ -17,3 +17,26 @@ const allClassElement = document.getElementsByClassName('info');
 
 const queryAll = document.querySelectorAll('.info');
 // console.log(queryAll[0].innerText);
+// noe vs Element DOM
+const container2 = document.querySelector('.container2');
+
+// parentElement
+/**
+ * nextElementSibling vs nextSibling
+ * previousElementSibling
+ * */ 
+
+const item2 = document.getElementById('item-2');
+
+// container4;
+
+// innerText || textContent || innerHTML;
+const container4 = document.querySelector('.container4');
+// console.log(container4.innerText);
+// console.log(container4.textContent);
+// console.log(container4.innerHTML);
+
+const myBtn = document.getElementById('myButton');
+myBtn.setAttribute('class', 'btn btn-primary btn-bg')
+
+myBtn.setAttribute('disabled', true);
---
+++ js/test.js
@@ -0,0 +1,5 @@
+const item2 = document.getElementById('item-2');
+const parent = item2.parentElement;
+const nextSibling = item2.nextElementSibling;
+const previous = item2.previousElementSibling;
+// console.log(previous);
